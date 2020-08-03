382594af6c1cfaaecf99a0589231ddea9794fc9e
uwaisalqadri <uwaisalqadri654321@gmail.com>
2020-08-03 11:11:43 +0700

    add a button to show notification and add pendingIntent to intent into a dicoding.com link

--- app/src/main/java/com/masuwes/androidnotification/MainActivity.kt
+++ app/src/main/java/com/masuwes/androidnotification/MainActivity.kt
@@ -2,11 +2,15 @@ package com.masuwes.androidnotification
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 
@@ -22,9 +26,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+    }
+
+    fun sendNotification(view: View) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://dicoding.com"))
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentIntent(pendingIntent)
                 .setSmallIcon(R.drawable.ic_notif)
                 .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_notif))
                 .setContentTitle(resources.getString(R.string.content_title))
